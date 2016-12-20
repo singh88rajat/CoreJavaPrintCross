c18ae579629c5cb2361f98908aac96ac3fa8b7bd
Mukesh Kumar <singh88rajat@gmail.com>
2016-12-20 22:26:54 +0530

    CoreJavaPrintCross

---
+++ src/CoreJavaPrintCross.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CoreJavaPrintCross {
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter the number of rows=column");
+        int n = s.nextInt();
+        int i, j;
+        s.close();
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (j == i) {
+                    System.out.print("*");
+                } else if (j == n - (i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
